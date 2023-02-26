bb845a77a29b62678ee7545d66cdbf42c472ea1c
Igor <goryanoy2002@gmail.com>
2023-02-26 16:20:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+import json
+
+my_db_name = 'db.json'
+
+
+def load_info(db_name):
+    with open(db_name, 'r') as db_file:
+        db_info = json.load(db_file)
+    return db_info['messages']
+
+
+def save_info(db_name, db_info):
+    data = {"messages": db_info}
+    with open(db_name, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+app = Flask(__name__)
+all_messages = load_info(my_db_name)
+
+
+@app.route('/')
+def index_page():
+    return "You found me"
+
+
+@app.route('/chat')
+def display_chat():
+    return render_template('form.html')
+
+
+@app.route('/get_messages')
+def get_message():
+    return {"messages": all_messages}
+
+@app.route('/send_message')
+def send_message():
+    sender = request.args['name']
+    text = request.args['text']
+    time = datetime.now().strftime("%H:%M")
+    text_info = \
+        {
+            "text": text,
+            "sender": sender,
+            "time": time
+        }
+    all_messages.append(text_info)
+    save_info(my_db_name, all_messages)
+    return {"messages": all_messages}
+
+
+app.run(host='0.0.0.0', port=4567)
